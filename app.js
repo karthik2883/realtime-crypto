fb75aad1ea39ca5f73be3744617c3060a56f2ba9
karthik_uandme <karthik.b@uandme.org>
2018-01-04 19:32:51 +0530

    latest changes

--- app.js
+++ app.js
@@ -1,39 +1,18 @@
 var http = require("http");
 var url = require('url');
 var fs = require('fs');
-
-var app = http.createServer(function (request, response) {
-    var path = url.parse(request.url).pathname;
-    switch (path) {
-        case '/':
-            response.writeHead(200, {
-                'Content-Type': 'text/html'
-            });
-            response.write('<h1>Hey, have you heard about our <a href="/signup.html">signup page</a></h1>');
-            response.end();
-            break;
-        case '/signup.html':
-            fs.readFile(__dirname + '/view/' + path, function (error, data) {
-                if (error) {
-                    response.writeHead(404);
-                    response.write("opps this doesn't exist - 404");
-                    response.end();
-                } else {
-                    response.writeHead(200, {
-                        "Content-Type": "text/html"
-                    });
-                    response.write(data, "utf8");
-                    response.end();
-                }
-            });
-            break;
-        default:
-            response.writeHead(404);
-            response.write("opps this doesn't exist - 404");
-            response.end();
-            break;
-    }
-});
-app.listen(8000);
-module.exports = app;
+var express = require('express');
+app = express();
+app.use(express.static(__dirname + '/view'));
  
+var api = express.Router();
+
+api.put('/', function (req, res) {
+   console.log("im here");
+    res.sendFile('./signup.html')
+});
+
+
+var server = http.createServer(app);
+server.listen(8000);
+module.exports = server;
--- server.js
+++ server.js
@@ -5,29 +5,18 @@ var appConstant = require('./config/appConstant');
 var bittrex = require('node-bittrex-api');
 var custom_func  = require('./utils/custom_function');
 var custom_func = new custom_func();
- /*// var io = require('socket.io').listen(server);
-// //turn off debug
-// //io.set('log level', 1);
-// // define interactions with client
-io.sockets.on('connection', function (socket) {
-    //send data to client
-    setInterval(function () {
-        socket.emit('stream', {
-            'title': "A new title via Socket.IO!"
-        });
-    }, 1000);
-// });*/
+ var ticker =[] ; 
 bittrex.websockets.client(function() {
      bittrex.websockets.listen(function(data, client) {
       if (data.M === 'updateSummaryState') {
-        data.A.forEach(function(data_for) {
+          data.A.forEach(function(data_for) {
             data_for.Deltas.forEach(function(marketsDelta) {
              var s = custom_func.saveTicker(marketsDelta).then(function(e){
-               var ticker  = e;
-              io.sockets.on('connection', function (socket) {
+               ticker.push(e);
+               io.sockets.on('connection', function (socket) {
                 //send data to client
                   socket.emit('stream', {
-                        'title:':ticker['MarketName']
+                        'value:':ticker
                    });
                 
               });
