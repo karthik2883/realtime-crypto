75d2dd6111d339c39c1483bf0518c3f48752c97e
karthik_uandme <karthik.b@uandme.org>
2018-01-11 17:28:15 +0530

    changes added

--- app.js
+++ app.js
@@ -2,18 +2,17 @@ const http = require("http");
 const process = require("process");
 const url = require('url');
 const fs = require('fs');
-var express = require('express');
+const express = require('express');
 const path = require('path');
 const app = express();
 const engine = require('consolidate');
+
 app.use(express.static(path.join(__dirname, 'view')));
 app.set('views', __dirname + '/view');
 app.engine('html', engine.mustache);
 app.set('view engine', 'html');
 app.get('/', function (req, res) {
-   //  console.log("im here");
-    // res.render("signup");
-    res.render('signup.html')
+   res.render('index.html')
 });
 process.setMaxListeners(0);
 const server = http.createServer(app);
--- server.js
+++ server.js
@@ -3,32 +3,30 @@ const app = require('./app');
 const io = require('socket.io').listen(app);
 const appConstant = require('./config/appConstant');
 const bittrex = require('node-bittrex-api');
-var custom_func  = require('./utils/custom_function');
+var custom_func = require('./utils/custom_function');
 
 var custom_func = new custom_func();
-var ticker =[] ; 
-bittrex.websockets.client(function() {
-     bittrex.websockets.listen(function(data, client) {
-      io.sockets.on('connection', function (socket) {
+var ticker = [];
+io.sockets.on('connection', frwsocket);
+
+function frwsocket(frwstrike) {
+  bittrex.websockets.client(function () {
+    bittrex.websockets.listen(function (data, client) {
       if (data.M === 'updateSummaryState') {
-          data.A.forEach(function(data_for) {
+        data.A.forEach(function (data_for) {
           console.log(data_for);
-            data_for.Deltas.forEach(function(marketsDelta) {
-              var s = custom_func.saveTicker(marketsDelta).then(function(e){
-               ticker.push(e);
-           
-                //send data to client
-                //setImmediate(function(){
-                  socket.emit('stream', {
-                        'value:':ticker
-                   });
-                  //},1000) 
-            }).catch(function(err){
-                 
+          data_for.Deltas.forEach(function (marketsDelta) {
+            var s = custom_func.saveTicker(marketsDelta).then(function (e) {
+              frwstrike.emit('stream', {
+                'value:':e
+              });
+            }).catch(function (err) {
+              console.log(err);
             });
           });
         });
       }
     });
-    });
+
   });
+}
